f91dd4b4a0d8166d189742636d3a37de136420bc
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2023-01-06 01:29:52 +0900

    feat : 회원가입 인증번호 전송

---
+++ finalproject/src/main/java/com/ggt/finalproject/controller/EmailController.java
@@ -0,0 +1,23 @@
+package com.ggt.finalproject.controller;
+
+import com.ggt.finalproject.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/emailCheck")
+    @ResponseBody
+    public String mailConfirm(@RequestParam String email) throws Exception {
+        String code = emailService.sendSimpleMessage(email);
+        log.info("인증코드 : " + code);
+        return code;
+    }
+}
