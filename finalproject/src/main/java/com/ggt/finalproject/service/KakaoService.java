5d7d58af8258e62edfceaff6e496488a5362ccc6
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2023-01-14 12:39:18 +0900

    fix : 프론트서버 배포주소로 변경

--- finalproject/src/main/java/com/ggt/finalproject/service/KakaoService.java
+++ finalproject/src/main/java/com/ggt/finalproject/service/KakaoService.java
@@ -67,7 +67,8 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "0a5a9b8a46f9a0836b9ff04d61ffc21c");
-        body.add("redirect_uri", "http://localhost:3000/user/kakao/callback");
+//        body.add("redirect_uri", "http://localhost:3000/user/kakao/callback");
+        body.add("redirect_uri", "https://dev.d134m2xe6xydy2.amplifyapp.com/user/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
