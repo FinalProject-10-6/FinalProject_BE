971868ef19e182c2a9f889b3b271605f605fbfbf
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2023-01-17 20:07:32 +0900

    Style : 에러메세지 id -> ID

--- finalproject/src/main/java/com/ggt/finalproject/exception/ErrorCode.java
+++ finalproject/src/main/java/com/ggt/finalproject/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST.value(),"비밀번호가 일치하지 않습니다."),
-    WRONG_ID(HttpStatus.NOT_FOUND.value(),"id 를 찾을 수 없습니다."),
+    WRONG_ID(HttpStatus.NOT_FOUND.value(),"ID 를 찾을 수 없습니다."),
     OVERLAPPED_LOGINID(HttpStatus.BAD_REQUEST.value(),"중복된 loginID 입니다."),
     OVERLAPPED_EMAIL(HttpStatus.BAD_REQUEST.value(),"중복된 email 입니다."),
     OVERLAPPED_NICKNAME(HttpStatus.BAD_REQUEST.value(),"중복된 nickname 입니다."),
