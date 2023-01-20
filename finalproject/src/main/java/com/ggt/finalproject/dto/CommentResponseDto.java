788875bce2a9e1eb1ba5bc7b3a2219a1c757a1ec
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2023-01-21 06:46:02 +0900

    fix : CommnetDto @getter 추가

--- finalproject/src/main/java/com/ggt/finalproject/dto/CommentResponseDto.java
+++ finalproject/src/main/java/com/ggt/finalproject/dto/CommentResponseDto.java
@@ -2,10 +2,13 @@ package com.ggt.finalproject.dto;
 
 import com.ggt.finalproject.entity.Comment;
 import com.ggt.finalproject.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
 public class CommentResponseDto {
 
     private Long id;
@@ -29,11 +32,4 @@ public class CommentResponseDto {
         this.content = comment.getContent();
         this.profileImg = comment.getProfileImg();
     }
-
-    public Long getId(){return this.id;}
-
-    public String getNickname(){return this.nickname;}
-
-    public String getContent(){return this.content;}
-
 }
