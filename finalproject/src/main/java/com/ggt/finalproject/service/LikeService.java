e25b9cf3fa07d75774ee1ccd8f008c7e0b9ca22a
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2023-02-02 20:29:14 +0900

    feat : 게시글 댓글, 좋아요시 알림기능

--- finalproject/src/main/java/com/ggt/finalproject/service/LikeService.java
+++ finalproject/src/main/java/com/ggt/finalproject/service/LikeService.java
@@ -4,6 +4,7 @@ package com.ggt.finalproject.service;
 import com.ggt.finalproject.dto.LikeResponseDto;
 import com.ggt.finalproject.dto.MsgResponseDto;
 import com.ggt.finalproject.entity.LikePost;
+import com.ggt.finalproject.entity.NotificationType;
 import com.ggt.finalproject.entity.Post;
 import com.ggt.finalproject.entity.User;
 import com.ggt.finalproject.exception.CustomException;
@@ -14,12 +15,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class LikeService {
 
     private final LikePostRepository likePostRepository;
     private final PostRepository postRepository;
+    private final NotificationService notificationService;
 
     @Transactional
     public LikeResponseDto likePost(Long postid, User user) {
@@ -32,6 +36,17 @@ public class LikeService {
             LikePost likePost = new LikePost(post, user);
             likePostRepository.save(likePost);
             post.setLikePostSum(post.getLikePostSum() + 1);
+
+            //해당 댓글로 이동하는 url
+            String Url = "https://tom-jelly.shop/api/post/"+post.getId();
+            //댓글 생성 시 모집글 작성 유저에게 실시간 알림 전송 ,
+            String content = post.getUser().getNickname()+"님! "+user.getNickname()+"님이 게시글에 좋아요을 눌렀습니다!";
+
+            //본인의 게시글에 좋아요을 남길때는 알림을 보낼 필요가 없다.
+            if(!Objects.equals(user.getId(), post.getUser().getId())) {
+                notificationService.send(post.getUser(), NotificationType.REPLY, content, Url);
+            }
+
             return LikeResponseDto.success("좋아요 추가", "true",post.getLikePostSum());
         } else {
             LikePost likePost = likePostRepository.findByPostAndUser(post, user);
